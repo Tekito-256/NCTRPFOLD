df1d9934d7c43a854cd51a338e23ea8a203ccedc
Maru <pikaio438@gmail.com>
2021-11-25 19:48:44 +0900

    Update !!

---
+++ Sources/main.c
@@ -0,0 +1,44 @@
+#include <3ds/svc.h>
+#include <3ds/srv.h>
+#include <3ds/types.h>
+#include "plgldr.h"
+#include "common.h"
+
+static Handle       thread;
+static u8           stack[STACK_SIZE] ALIGN(8);
+
+void     Flash(u32 color)
+{
+  color |= 0x01000000;
+  for (u32 i = 0; i < 64; i++)
+  {
+    REG32(0x10202204) = color;
+    svcSleepThread(5000000);
+  }
+  REG32(0x10202204) = 0;
+}
+
+// Plugin main thread entrypoint
+void    ThreadMain(void* arg)
+{
+  // Plugin main loop
+  while (1)
+  {
+    // Check keys, display the menu if necessary
+    if (HID_PAD & BUTTON_SELECT)
+      Flash(0x00FF00);
+  }
+}
+
+void main(void)
+{
+  PluginHeader *header = (PluginHeader *)(0x07000000);
+  if (header->magic != HeaderMagic)
+    return;
+
+  // 3ds/srv.h
+  srvInit();
+
+  svcCreateThread(&thread, ThreadMain, 0, (u32*)(stack + STACK_SIZE), 30, -1);
+  return;
+}
